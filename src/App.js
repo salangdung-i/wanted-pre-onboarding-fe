cc6deb7323db2821fc2d4a94bbc8aa6196e5a0f1
salangdung-i <dladmswl92@naver.com>
2022-06-26 21:52:17 +0900

    추가: 라우터

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import PreAssignmentGuide from './pages/PreAssignmentGuide';
+import { Route, Routes } from 'react-router';
+import Main from './pages/main';
+import Login from './pages/login';
 
 function App() {
-  return <PreAssignmentGuide />;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/main" element={<Main />} />
+    </Routes>
+  );
 }
 
 export default App;
